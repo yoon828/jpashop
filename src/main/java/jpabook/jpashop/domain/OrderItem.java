a1057701f3e574e3a95c70bdbd32dd5ec1a0b2f9
yoon <yoon828990@gmail.com>
2023-02-23 20:37:41 +0900

    엔티티 설정

---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
+
+@Entity
+@Table(name = "order_item")
+@Getter @Setter
+public class OrderItem {
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+    private int orderPrice; //주문 가격
+    private int cnt; //주문 수량
+}
